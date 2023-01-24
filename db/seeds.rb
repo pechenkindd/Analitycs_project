3bfc41446f7645c434550748cb0d76a032f5e094
danila <dan1981505@gmail.com>
2023-01-24 06:41:01 +0300

    1.0

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,18 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-(6..50).each do |i| 
-	New.update(i,
+50.times do |i| 
+	New.create(i,
 		topic: Faker::Lorem.sentence(word_count: 5, random_words_to_add: 2),
 		body: Faker::Lorem.paragraph(sentence_count: 50, random_sentences_to_add: 2),
 		theme_id: rand(1..5)
 	)
 end
+
+# (6..50).each do |i| 
+# 	New.update(i,
+# 		topic: Faker::Lorem.sentence(word_count: 5, random_words_to_add: 2),
+# 		body: Faker::Lorem.paragraph(sentence_count: 50, random_sentences_to_add: 2),
+# 		theme_id: rand(1..5)
+# 	)
+# end
